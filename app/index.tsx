d74b5dba45dd902e073020b79b5f805cf643372d
Hamza <142801742+HamzaIshfaq889@users.noreply.github.com>
2025-03-29 13:59:43 +0500

    plain expo project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Homescreen = () => {
+  return (
+    <Text className=''>Homescreen</Text>
+  )
+}
+
+export default Homescreen
