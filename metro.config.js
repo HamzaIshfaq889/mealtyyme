15b66f23b17a314f1371bcc959bb244298e89e84
HamzaIshfaq889 <hamzaishfaq889@gmail.com>
2025-05-04 20:09:31 +0500

    Upgrade to sdk expo go 53

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,8 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.unstable_enablePackageExports = false
+
 config.transformer.babelTransformerPath = require.resolve(
   "react-native-svg-transformer"
 );
